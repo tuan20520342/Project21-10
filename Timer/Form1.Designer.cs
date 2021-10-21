2507df08e1e85ad849c5ea88ff366ef38f743ace
quangSotech <71537969+akihiro22@users.noreply.github.com>
2021-10-21 09:38:06 +0700

    timer cs

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDuration = new System.Windows.Forms.TextBox();
+            this.cbAction = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDo = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Duration";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Action";
+            // 
+            // txtDuration
+            // 
+            this.txtDuration.Location = new System.Drawing.Point(170, 62);
+            this.txtDuration.Name = "txtDuration";
+            this.txtDuration.Size = new System.Drawing.Size(174, 22);
+            this.txtDuration.TabIndex = 2;
+            // 
+            // cbAction
+            // 
+            this.cbAction.FormattingEnabled = true;
+            this.cbAction.Items.AddRange(new object[] {
+            "Shutdown (default)",
+            "Log out",
+            "Restart",
+            "Sleep"});
+            this.cbAction.Location = new System.Drawing.Point(170, 111);
+            this.cbAction.Name = "cbAction";
+            this.cbAction.Size = new System.Drawing.Size(174, 24);
+            this.cbAction.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(350, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "(m)";
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(213, 198);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(121, 43);
+            this.btnDo.TabIndex = 5;
+            this.btnDo.Text = "Do";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(349, 198);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(121, 43);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Time remaining";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(175, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(249, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "(s)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 253);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbAction);
+            this.Controls.Add(this.txtDuration);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Time remaining Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDuration;
+        private System.Windows.Forms.ComboBox cbAction;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        static int timeInput=0;
+        public Form1()
+        {
+            InitializeComponent();
+            cbAction.SelectedIndex = 0;
+            btnCancel.Enabled = false;
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                timeInput = int.Parse(txtDuration.Text) * 60;
+                txtDuration.Enabled = false;
+                cbAction.Enabled = false;
+                if (timeInput < 0)
+                    {
+                        throw new Exception("Vui lòng nhập vào một số nguyên dương");
+                    }
+                else
+                {
+                    if (!timer1.Enabled)
+                    {
+                        timer1.Start();
+                        btnDo.Enabled = false;
+                        btnCancel.Enabled = true;
+                    }         
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Vui lòng nhập vào một số nguyên dương");
+                txtDuration.Text = string.Empty;
+                label5.Text = string.Empty;
+            }
+            catch (OverflowException)
+            {
+                    MessageBox.Show("Số vừa nhập quá lớn.");
+                    txtDuration.Text = string.Empty;
+                    label5.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                txtDuration.Text = string.Empty;
+                label5.Text = string.Empty;
+            }
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chương trình đã được cancel thành công.");
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                label5.Text = "0";
+                btnDo.Enabled = true;
+                btnCancel.Enabled = false;
+                txtDuration.Enabled = true;
+                cbAction.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeInput--;
+            label5.Text = timeInput.ToString();
+            if(timeInput==0)
+            {
+                timer1.Stop();
+                string filename = string.Empty;
+                string arguments = string.Empty;
+                if(cbAction.SelectedIndex==0)
+                {
+                    filename = "shutdown.exe";
+                    arguments = "-s";
+                }
+                else if(cbAction.SelectedIndex == 1)
+                {
+                    filename = "shutdown.exe";
+                    arguments = "-l";
+                }
+                else if(cbAction.SelectedIndex == 2)
+                {
+
+                    filename = "shutdown.exe";
+                    arguments = "-r";
+                }
+                else if(cbAction.SelectedIndex == 3)
+                {
+                    filename = "Rundll32.exe";
+                    arguments = "powrprof.dll, SetSuspendState 0,1,0";
+                }
+                ProcessStartInfo startinfo = new ProcessStartInfo(filename, arguments);
+                Process.Start(startinfo);
+            }
+        }
+    }
+}
